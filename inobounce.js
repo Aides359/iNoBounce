4e3236b79823f9e78f88fa65fd123eb7df652108
Blessing Pascal <pascal.blessing@gmx.at>
2017-02-02 16:30:15 +0100

    Allow X scrolling if relaxed class applies (this can cause bounce)

--- inobounce.js
+++ inobounce.js
@@ -1,121 +1,163 @@
-/*! iNoBounce - v0.1.0
-* https://github.com/lazd/iNoBounce/
-* Copyright (c) 2013 Larry Davis <lazdnet@gmail.com>; Licensed BSD */
+/*! iNoBounce - v0.1.2
+ * https://github.com/lazd/iNoBounce/
+ * Copyright (c) 2013 Larry Davis <lazdnet@gmail.com>; Licensed BSD */
 (function(global) {
-	// Stores the Y position where the touch started
-	var startY = 0;
+    // Stores the X & Y position where the touch started
+    var startY = 0;
+    var startX = 0;
 
-	// Store enabled status
-	var enabled = false;
+    // Store enabled status
+    var enabled = false;
 
-	var handleTouchmove = function(evt) {
-		// Get the element that was scrolled upon
-		var el = evt.target;
+    // List of classes that allow X scrolling on element
+    var relaxedClasses = [];
 
-		// Check all parent elements for scrollability
-		while (el !== document.body) {
-			// Get some style properties
-			var style = window.getComputedStyle(el);
+    var handleTouchmove = function(evt) {
+        // Get the element that was scrolled upon
+        var el = evt.target;
 
-			if (!style) {
-				// If we've encountered an element we can't compute the style for, get out
-				break;
-			}
 
-			// Ignore range input element
-			if (el.nodeName === 'INPUT' && el.getAttribute('type') === 'range') {
-				return;
-			}
+        // Check all parent elements for scrollability
+        while (el !== document.body) {
+            // Get some style properties
+            var style = window.getComputedStyle(el);
 
-			var scrolling = style.getPropertyValue('-webkit-overflow-scrolling');
-			var overflowY = style.getPropertyValue('overflow-y');
-			var height = parseInt(style.getPropertyValue('height'), 10);
+            if (!style) {
+                // If we've encountered an element we can't compute the style for, get out
+                break;
+            }
 
-			// Determine if the element should scroll
-			var isScrollable = scrolling === 'touch' && (overflowY === 'auto' || overflowY === 'scroll');
-			var canScroll = el.scrollHeight > el.offsetHeight;
+			// Check if element has one of the relaxed classes
+            var strict = !(relaxedClasses.length > 0 && relaxedClasses.some(function(value) { return el.classList.contains(value) }))
 
-			if (isScrollable && canScroll) {
-				// Get the current Y position of the touch
-				var curY = evt.touches ? evt.touches[0].screenY : evt.screenY;
+            var scrolling = style.getPropertyValue('-webkit-overflow-scrolling');
 
-				// Determine if the user is trying to scroll past the top or bottom
-				// In this case, the window will bounce, so we have to prevent scrolling completely
-				var isAtTop = (startY <= curY && el.scrollTop === 0);
-				var isAtBottom = (startY >= curY && el.scrollHeight - el.scrollTop === height);
+            // Handle Y
+            var overflowY = style.getPropertyValue('overflow-y');
+            var height = parseInt(style.getPropertyValue('height'), 10);
 
-				// Stop a bounce bug when at the bottom or top of the scrollable element
-				if (isAtTop || isAtBottom) {
-					evt.preventDefault();
-				}
+            // Determine if the element should scroll in Y
+            var isYScrollable = scrolling === 'touch' && (overflowY === 'auto' || overflowY === 'scroll');
+            var canYScroll = el.scrollHeight > el.offsetHeight;
 
-				// No need to continue up the DOM, we've done our job
-				return;
-			}
+            if (!strict) {
+                // Handle X
+                var overflowX = style.getPropertyValue('overflow-x');
+                var width = parseInt(style.getPropertyValue('width'), 10);
 
-			// Test the next parent
-			el = el.parentNode;
-		}
+                // Determine if the element should scroll in X
+                var isXScrollable = scrolling === 'touch' && (overflowX === 'auto' || overflowX === 'scroll');
+                var canXScroll = el.scrollWidth > el.offsetWidth;
+            }
 
-		// Stop the bouncing -- no parents are scrollable
-		evt.preventDefault();
-	};
+            if ((isYScrollable && canYScroll) || (!strict && (isXScrollable && canXScroll))) {
+                var curY, curX;
+                var isAtTop, isAtBottom, isAtLeft, isAtRight;
 
-	var handleTouchstart = function(evt) {
-		// Store the first Y position of the touch
-		startY = evt.touches ? evt.touches[0].screenY : evt.screenY;
-	};
+                // Get the current Y position of the touch
+                curY = evt.touches ? evt.touches[0].screenY : evt.screenY;
 
-	var enable = function() {
-		// Listen to a couple key touch events
-		window.addEventListener('touchstart', handleTouchstart, false);
-		window.addEventListener('touchmove', handleTouchmove, false);
-		enabled = true;
-	};
+                // Determine if the user is trying to scroll past the top or bottom
+                // In this case, the window will bounce, so we have to prevent scrolling completely
+                isAtTop = (startY <= curY && el.scrollTop === 0);
+                isAtBottom = (startY >= curY && el.scrollHeight - el.scrollTop === height);
 
-	var disable = function() {
-		// Stop listening
-		window.removeEventListener('touchstart', handleTouchstart, false);
-		window.removeEventListener('touchmove', handleTouchmove, false);
-		enabled = false;
-	};
+                if (!strict) {
+                    // Get the current X position of the touch
+                    curX = evt.touches ? evt.touches[0].screenX : evt.screenX;
 
-	var isEnabled = function() {
-		return enabled;
-	};
+                    // Determine if the user is trying to scroll past the left or right
+                    // In this case, the window will bounce, so we have to prevent scrolling completely
+                    isAtLeft = (startX <= curX && el.scrollLeft === 0);
+                    isAtRight = (startX >= curX && el.scrollWidth - el.scrollLeft === width);
+                }
 
-	// Enable by default if the browser supports -webkit-overflow-scrolling
-	// Test this by setting the property with JavaScript on an element that exists in the DOM
-	// Then, see if the property is reflected in the computed style
-	var testDiv = document.createElement('div');
-	document.documentElement.appendChild(testDiv);
-	testDiv.style.WebkitOverflowScrolling = 'touch';
-	var scrollSupport = 'getComputedStyle' in window && window.getComputedStyle(testDiv)['-webkit-overflow-scrolling'] === 'touch';
-	document.documentElement.removeChild(testDiv);
+                // Stop a bounce bug when at the bottom or top of the scrollable element or at left or right if not in strict mode
+                if ((isAtTop || isAtBottom) && (strict || isAtLeft || isAtRight)) {
+                    evt.preventDefault();
+                }
 
-	if (scrollSupport) {
-		enable();
-	}
+                // No need to continue up the DOM, we've done our job
+                return;
+            }
 
-	// A module to support enabling/disabling iNoBounce
-	var iNoBounce = {
-		enable: enable,
-		disable: disable,
-		isEnabled: isEnabled
-	};
+            // Test the next parent
+            el = el.parentNode;
+        }
+        // Stop the bouncing -- no parents are scrollable
+        evt.preventDefault();
+    };
 
-	if (typeof module !== 'undefined' && module.exports) {
-		// Node.js Support
-		module.exports = iNoBounce;
-	}
-	if (typeof global.define === 'function') {
-		// AMD Support
-		(function(define) {
-			define('iNoBounce', [], function() { return iNoBounce; });
-		}(global.define));
-	}
-	else {
-		// Browser support
-		global.iNoBounce = iNoBounce;
-	}
+    var handleTouchstart = function(evt) {
+        // Store the first X & Y position of the touch
+        startY = evt.touches ? evt.touches[0].screenY : evt.screenY;
+        startX = evt.touches ? evt.touches[0].screenX : evt.screenX;
+    };
+
+    var enable = function() {
+        // Listen to a couple key touch events
+        window.addEventListener('touchstart', handleTouchstart, false);
+        window.addEventListener('touchmove', handleTouchmove, false);
+        enabled = true;
+    };
+
+    var disable = function() {
+        // Stop listening
+        window.removeEventListener('touchstart', handleTouchstart, false);
+        window.removeEventListener('touchmove', handleTouchmove, false);
+        enabled = false;
+    };
+
+    var isEnabled = function() {
+        return enabled;
+    };
+
+    var addRelaxedClass = function(className) {
+        if (relaxedClasses.indexOf(className) === -1) {
+            relaxedClasses.push(className);
+        }
+    }
+
+    var removeRelaxedClass = function(className) {
+        var index = relaxedClasses.indexOf(className);
+        if (index > -1) {
+            relaxedClasses.splice(index, 1);
+        }
+    }
+
+    // Enable by default if the browser supports -webkit-overflow-scrolling
+    // Test this by setting the property with JavaScript on an element that exists in the DOM
+    // Then, see if the property is reflected in the computed style
+    var testDiv = document.createElement('div');
+    document.documentElement.appendChild(testDiv);
+    testDiv.style.WebkitOverflowScrolling = 'touch';
+    var scrollSupport = 'getComputedStyle' in window && window.getComputedStyle(testDiv)['-webkit-overflow-scrolling'] === 'touch';
+    document.documentElement.removeChild(testDiv);
+
+    if (scrollSupport) {
+        enable();
+    }
+
+    // A module to support enabling/disabling iNoBounce
+    var iNoBounce = {
+        enable: enable,
+        disable: disable,
+        isEnabled: isEnabled,
+        addRelaxedClass: addRelaxedClass,
+        removeRelaxedClass: removeRelaxedClass
+    };
+
+    if (typeof module !== 'undefined' && module.exports) {
+        // Node.js Support
+        module.exports = iNoBounce;
+    }
+    if (typeof global.define === 'function') {
+        // AMD Support
+        (function(define) {
+            define(function() { return iNoBounce; });
+        }(global.define));
+    } else {
+        // Browser support
+        global.iNoBounce = iNoBounce;
+    }
 }(this));
